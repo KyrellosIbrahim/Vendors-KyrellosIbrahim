f004d6b09a6aa49027110f84c557f1a910dc4648
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2024-11-07 10:16:49 -0600

    addMoney test

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -1,12 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendorTest {
+    static Vending v;
+
+    @BeforeEach
+    void setUp() {
+        v = new Vending(0, 0);
+    }
 
     @Test
     void addition() {
         assertEquals(2, 1 + 1);
     }
+    @Test
+    void addMoney() {
+        v.addMoney(1.0);
+        assertEquals(1.0, v.getBalance());
+    }
+
 
 }
